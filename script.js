d3545aa11df9cd5cb709f4a7ed31198513461d62
CristiDan <dan.cristian2906@gmail.com>
2023-04-03 16:50:27 +0300

    Final features

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+// creating objects of all relevant elements of the page
 const form = document.getElementById('form1');
 const form2 = document.getElementById("form2")
 const input1 = document.getElementById("gridInput")
 const input2 = document.getElementById("pairs")
-
+const textArea = document.getElementById("textArea");
+const numbersDisplay = document.getElementById("numbersDisplay")
 
 const next = document.getElementById("nextButton")
 const prev = document.getElementById("prevButton")
@@ -15,8 +17,9 @@ const ctx = canvas.getContext('2d');
 
 const display = document.getElementById('displayBox');
 
+// global variable declaration
 let gridSize;
-
+let nrPuncte;
 let pointsArray = [];
 let lineWidth;
 let lineHeight;
@@ -24,53 +27,21 @@ let lineHeight;
 let xSize;
 let ySize;
 let grid = [];
-
-
-
-function matrixRepresentation(array) {
-    let matrix = [];
-    let index = 0;
-    for (let i = 0; i < array.length / 2; i++) {
-        matrix[i] = [];
-        for (var j = 0; j < 2; j++) {
-            matrix[i][j] = parseInt(array[index]);
-            index++;
-        }
-    }
-    return matrix;
-
-}
-
 let pointsMatrix = [];
+let rectCounter = 0;
+let intArray = []
 
 
-form1.addEventListener('submit', (event) => {
-    event.preventDefault();
+
+
+
+// function that draws the grid and the points
+function desen()
+{
     ctx.clearRect(0, 0, canvasHeight, canvasWidth);
-
-    gridSize = input1.value;
-
-    let intArray = gridSize.split(',');
-
-
-
-    let points = input2.value;
-
-    let k = 0;
-    for (let i = 0; i <= points.length; i++) {
-        if (points[i] == parseInt(points[i])) {
-            pointsArray[k] = points[i]
-            k++;
-        }
-
-    }
-    // console.log(pointsArray)
-
     pointsMatrix = matrixRepresentation(pointsArray);
-
+    //console.log(pointsMatrix)
     nrPuncte = pointsMatrix.length;
-
-    // console.log(pointsMatrix.length);
     xSize = parseInt(intArray[0])
     ySize = parseInt(intArray[1]);
     lineWidth = canvasWidth / ySize;
@@ -81,17 +52,16 @@ form1.addEventListener('submit', (event) => {
     for (let i = 0; i < xSize; i++) {
         ctx.lineWidth = 1;
         ctx.strokeStyle = 'grey'
-        //ctx.setLineDash([2, 2]);
         ctx.beginPath();
         ctx.moveTo(0, lineHeight * i);
         ctx.lineTo(canvasWidth, lineHeight * i);
         ctx.stroke();
     }
 
+    // Vertical lines of the grid
     for (let i = 0; i < ySize; i++) {
         ctx.lineWidth = 1;
         ctx.strokeStyle = 'grey'
-        // ctx.setLineDash([2, 2]);
         ctx.beginPath();
         ctx.moveTo(lineWidth * i, 0);
         ctx.lineTo(lineWidth * i, canvasHeight);
@@ -105,104 +75,32 @@ form1.addEventListener('submit', (event) => {
         ctx.arc(pointsMatrix[i][1] * lineWidth, pointsMatrix[i][0] * lineHeight, lineWidth / 10, 0, 2 * Math.PI)
         ctx.fill();
     }
-
-});
-
-
-
-
-
-
-
-let rectCounter = 0;
-
-next.addEventListener("click", () => {
-    rectCounter++;
-    if (rectCounter < rectangleArray.length) {
-        sterge(...rectangleArray[rectCounter - 1])
-        draw(...rectangleArray[rectCounter])
-
-    }
-    else {
-        sterge(...rectangleArray[rectangleArray.length - 1])
-        rectCounter = 0;
-        draw(...rectangleArray[rectCounter])
-
-    }
-
-    console.log(rectCounter)
-
-
-})
-
-
-prev.addEventListener("click", () => {
-    rectCounter--;
-    if (rectCounter == rectangleArray.length) {
-        sterge(...rectangleArray[rectangleArray.length - 1])
-        rectCounter = 0;
-        draw(...rectangleArray[rectCounter])
-
-    }
-    else if (rectCounter <= 0) {
-        sterge(...rectangleArray[rectangleArray.length - 1])
-        rectCounter = rectangleArray.length;
-        draw(...rectangleArray[rectCounter - 1])
-
-    }
-    else {
-        sterge(...rectangleArray[rectCounter - 1])
-        draw(...rectangleArray[rectCounter])
-    }
-
-
-    console.log(rectCounter)
-
-})
-
-
-
-function binaryInsertionSort(arr) {
-    for (let i = 1; i < arr.length; i++) {
-        let left = 0;
-        let right = i - 1;
-        const key = arr[i];
-        while (left <= right) {
-            const mid = Math.floor((left + right) / 2);
-            if (arr[mid][0] > key[0]) {
-                right = mid - 1;
-            } else {
-                left = mid + 1;
-            }
-        }
-        for (let j = i - 1; j >= left; j--) {
-            arr[j + 1] = arr[j];
-        }
-        arr[left] = key;
-    }
-    return arr;
 }
 
 
-
-function binarySearch(arr, start, end, target) {
-    while (start <= end) {
-        const mid = Math.floor((start + end) / 2);
-        if (arr[mid][0] < target[0]) {
-            start = mid + 1;
-        } else if (arr[mid][0] > target[0]) {
-            end = mid - 1;
-        } else if (arr[mid][1] < target[1]) {
-            start = mid + 1;
-        } else {
-            end = mid - 1;
+// function parses the array to be bi-dimensional array
+function matrixRepresentation(array) {
+    let matrix = [];
+    let index = 0;
+    for (let i = 0; i < array.length / 2; i++) {
+        matrix[i] = [];
+        for (var j = 0; j < 2; j++) {
+            matrix[i][j] = (array[index]);
+            index++;
         }
     }
-    return start;
+    return matrix;
+
 }
 
+// function to extract all numbers from a string
+function getNumbersFromString(str) {
+    const regex = /\d+/g;
+    const matches = str.match(regex); // array of all matches
+    return matches.map(Number);
+  }
 
-
+// searching for the node, linear O(n) complexity
 function cautare(arr, target, start = 0, end = arr.length - 1) {
     let left = start;
     let right = end;
@@ -214,144 +112,175 @@ function cautare(arr, target, start = 0, end = arr.length - 1) {
 
 }
 
-
-
-
-function sterge(c1, c2, c3, c4) {
-    console.log("Astea sterg:" + c1, c2, c3, c4);
-    ctx.strokeStyle = '#FFFFFF'
-    ctx.lineWidth = lineWidth / 30 + 2;
-    ctx.beginPath();
-    ctx.strokeRect(c1[0] * lineWidth, c1[1] * lineHeight, (c2[1] - c1[1]) * lineWidth, (c4[0] - c2[0]) * lineHeight)
-    console.log(c1[0] * lineWidth, c1[1] * lineHeight, (c2[1] - c1[1]) * lineWidth, (c4[0] - c2[0]) * lineHeight);
-
-
-
-    ctx.strokeStyle = 'grey'
-    ctx.lineWidth = 1;
-    ctx.beginPath();
-    ctx.strokeRect(c1[0] * lineWidth, c1[1] * lineHeight, (c2[1] - c1[1]) * lineWidth, (c4[0] - c2[0]) * lineHeight)
-
-    // ctx.beginPath();
-    // ctx.moveTo(c1[1] * lineWidth, c1[0] * lineHeight);
-    // ctx.lineTo(c2[1] * lineWidth, c2[0] * lineHeight);
-    // ctx.lineTo(c3[1] * lineWidth, c3[0] * lineHeight);
-    // ctx.lineTo(c4[1] * lineWidth, c4[0] * lineHeight);
-    // ctx.closePath();
-    // ctx.stroke();
-
-
-    for (let i = 0; i < nrPuncte; i++) {
-        ctx.fillStyle = 'blue'
-        ctx.beginPath();
-        ctx.arc(pointsMatrix[i][1] * lineWidth, pointsMatrix[i][0] * lineHeight, lineWidth / 10, 0, 2 * Math.PI)
-        ctx.fill();
-    }
-
-
-}
-
-
+// function used to display the rectangles
 function draw(c1, c2, c3, c4) {
-    console.log("Astea desenez " + c1, c2, c3, c4)
-    ctx.strokeStyle = 'black'
-    ctx.lineWidth = lineWidth / 30;
+    xSize = parseInt(intArray[0])
+    ySize = parseInt(intArray[1]);
+    lineWidth = canvasWidth / ySize;
+    lineHeight = canvasHeight / xSize;
+
+// redrawing the grid
+   desen();
+// drawing the rectangle
+    ctx.strokeStyle = 'red'
+    ctx.lineWidth = lineWidth / 15;
     ctx.setLineDash([]);
     ctx.beginPath();
+    // flipped coordinates as it was easier for me to work with them this way
     ctx.moveTo(c1[1] * lineWidth, c1[0] * lineHeight);
     ctx.lineTo(c2[1] * lineWidth, c2[0] * lineHeight);
     ctx.lineTo(c3[1] * lineWidth, c3[0] * lineHeight);
     ctx.lineTo(c4[1] * lineWidth, c4[0] * lineHeight);
     ctx.closePath();
     ctx.stroke();
-
-
-
-
-
 }
 
 
-
-function sortArray(arr) {
-    for (let i = 1; i < arr.length; i++) {
-        const curr = arr[i];
-        const index = binaryInsertionSort(arr, 0, i - 1, curr);
-        arr.splice(index, 0, curr);
-        arr.splice(i + 1, 1);
-    }
-
-    return arr;
-}
-
-
-
-let rectangleArray = [];
-let p = 0;
-
-function Rectangles(matrix) {
+// sorting the matrix by the x coordinate, ordering by the y coordinate when
+// the x's are equal
+function sortArray(matrix) {
     for (let i = 0; i < matrix.length; i++) {
         for (let j = i; j < matrix.length; j++) {
             if (matrix[i][0] > matrix[j][0]) {
-                matrix.splice(i, 0, j);
-                matrix.splice(i + 1, 1);
+                let ajutor = [];
+                ajutor[0] = matrix[j][0];
+                ajutor[1] = matrix[j][1];
+
+                matrix[j][0] = matrix[i][0];
+                matrix[j][1] = matrix[i][1];
+
+                matrix[i][0] = ajutor[0];
+                matrix[i][1] = ajutor[1];
+
+               
             }
-
-
             else if (matrix[i][0] == matrix[j][0]) {
-                if (matrix[i][1] > matrix[j][1])
-                    matrix.splice(i, 0, j);
-                matrix.splice(i + 1, 1);
+               if(matrix[i][1] > matrix[j][1])
+               {
+                let ajutor = [];
+                ajutor[0] = matrix[j][0];
+                ajutor[1] = matrix[j][1];
+
+                matrix[j][0] = matrix[i][0];
+                matrix[j][1] = matrix[i][1];
+
+                matrix[i][0] = ajutor[0];
+                matrix[i][1] = ajutor[1];
+
+               }
             }
         }
     }
-    //sortArray(matrix);
-    console.log(matrix)
+    return matrix;
+}
+
+
+let dreptString = [];
+let rectangleArray = [];
+let p = 0;
+
+
+// the bread and butter of the algorithm, complexity O(n^3)
+function Rectangles(matrix) {
+    p = 0;
+    // sorting the matrix
+    matrix = sortArray(matrix);
+    //console.log(matrix)
     let noRect = 0;
+
+    //iterating over the matrix to find possible opposite corners of a rectangle 
     for (let i = 0; i < matrix.length; i++) {
         for (let j = i; j < matrix.length; j++) {
             if (matrix[i][0] != matrix[j][0] && matrix[i][1] != matrix[j][1]) {
+                // initializing the other two vertices to look for
                 let targetNode1 = [matrix[i][0], matrix[j][1]];
                 let targetNode2 = [matrix[j][0], matrix[i][1]];
-                if (cautare(matrix, targetNode1) > i && cautare(matrix, targetNode2) > i) {
+                // searching for the vertices, which we know are in the interval [i,j] because the array is sorted
+                if (cautare(matrix, targetNode1,i+1,j) && cautare(matrix, targetNode2,i+1,j)) {
                     noRect++;
-                    console.log(`Colturile dreptunghiului ${noRect} sunt : ${matrix[i]} ${targetNode1} ${matrix[j]}  ${targetNode2}`)
+                    // adding text to the string that is displayed in the text box
+                    dreptString[p] = (`Current rectangle: (${matrix[i]}), (${targetNode1}), (${matrix[j]}), (${targetNode2})`)
+                    // saving the rectangle to an array
                     rectangleArray[p] = [matrix[i], targetNode1, matrix[j], targetNode2];
                     p++;
-
                 }
 
 
             }
         }
     }
-
-
-    // for (let i = 0; i < p; i++) draw(...rectangleArray[i]);
+    // drawing the first rectangle
     draw(...rectangleArray[0]);
+
+    // displaying the text about the number of found rectangles
+
     if (noRect === 0)
-        display.value = 'no rectangles found';
-    else if (noRect === 1) display.value = '1 rectangle found'
+        numbersDisplay.innerHTML = 'no rectangles found';
+    else if (noRect === 1)
+    {
+        numbersDisplay.innerHTML = '1 rectangle found'
+        textArea.value = dreptString[0];
+    } 
     else {
-        display.value = `${noRect} rectangles`
+        numbersDisplay.innerHTML = `${noRect} rectangles found`
+        textArea.value = dreptString[0];
     }
-    console.log(noRect);
+    console.log(noRect + " rectangles");
 }
 
 
-let nrPuncte;
 
 
+// event listener for the first button
+form1.addEventListener('submit', (event) => {
+    event.preventDefault();
+    
+    gridSize = input1.value;
+
+    // parsing the grid size
+    intArray = gridSize.split(',');
+   
+
+   // extracting the points from the second input
+        let points = input2.value;
+      pointsArray = getNumbersFromString(points);
+      desen();
+     
+    
+  
+  
+});
 
 
-
+// event listener for the 'Look for rectangles!' button
 form2.addEventListener('submit', (event) => {
-
+    rectCounter = 1;
     event.preventDefault();
 
-    let pointsMatrix = matrixRepresentation(pointsArray);
     Rectangles(pointsMatrix);
-
-
+    console.log(rectangleArray.length);
 })
+
+
+
+// event listener for the next button
+next.addEventListener("click", () => {
+    rectCounter++;
+    console.log(rectCounter)
+    if (rectCounter < rectangleArray.length) {
+        draw(...rectangleArray[rectCounter])
+        textArea.value = dreptString[rectCounter];
+    }
+    else if (rectCounter >= rectangleArray.length) {
+        rectCounter = 0;
+        console.log(rectCounter)
+        draw(...rectangleArray[rectCounter])
+        textArea.value = dreptString[rectCounter];
+    }
+})
+
+
+
+
+
 
